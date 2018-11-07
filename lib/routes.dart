11893a6224e3815e396e02c77cd6797f788885eb
Simon Hull <Simon@simonhull.com>
2018-11-07 10:58:06 -0700

    Scaffolded Hello World

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+import 'package:presently/screens/home.dart';
+
+var routes = <String, WidgetBuilder> {
+  'home': (BuildContext context) => HomeScreen()
+};

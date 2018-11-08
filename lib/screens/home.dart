46e22c2ef0b5c4897c6a0e24040cf138db98ab00
Jared Perfect <jared.perfect@gmail.com>
2018-11-08 09:54:16 -0700

    Added some content to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,11 +4,91 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Upcoming Events',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Chip(
+                      label: Text(
+                        'BIRTHDAY',
+                        style: TextStyle(color: Colors.white, fontSize: 12.0),
+                      ),
+                      backgroundColor: Colors.purple,
+                    ),
+                  ),
+                  ListTile(
+                      leading: CircleAvatar(
+                          backgroundColor: Colors.lightBlue,
+                          radius: 32.0,
+                          child: Text('FA',
+                              style: TextStyle(color: Colors.white))),
+                      title: Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(
+                          'Fred Arden',
+                          style: TextStyle(
+                              fontSize: 24.0, fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                      subtitle: Text(
+                        'Give Fred the perfect gift!',
+                        style: TextStyle(fontSize: 20.0),
+                      )),
+                  ButtonTheme.bar(
+                    child: ButtonBar(
+                      alignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        FlatButton(
+                          child: Text(
+                            'GIFT',
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          onPressed: () => {},
+                        ),
+                        FlatButton(
+                          child:
+                              Text('PROFILE', style: TextStyle(fontSize: 16.0)),
+                          onPressed: () => {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'My Wishlist',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text('No items currently in your wishlist'))
+          ],
         ),
-      body: Center(
-        child: Text("Hello World"),
       ),
     );
   }

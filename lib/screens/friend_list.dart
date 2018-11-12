fd7463905e0855273a70b7e1c4f636c0253a5238
Jared Perfect <jared.perfect@gmail.com>
2018-11-12 11:03:10 -0700

    Create friend list and redrect to add friend page

--- lib/screens/friend_list.dart
+++ lib/screens/friend_list.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:presently/screens/add_friend.dart';
 
 class FriendListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cart'),
+        title: Text('Friends'),
       ),
-      body: Column(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddFriend()),
+          );
+        },
+        backgroundColor: Colors.purple,
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: Text('JD'),
+              ),
+              title: Text('Jane Doe'),
+              subtitle: Text('Birthday: January 15th'),
+              trailing: Icon(Icons.card_giftcard),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

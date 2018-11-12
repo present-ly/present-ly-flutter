558b8031ca827dd5a502120a40cce92f98c56fee
Jared Perfect <jared.perfect@gmail.com>
2018-11-12 14:12:49 -0700

    Create bare-bones 'add friend' form.

--- lib/screens/add_friend.dart
+++ lib/screens/add_friend.dart
@@ -4,7 +4,42 @@ class AddFriend extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(title: Text('Add New Friend')),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Form(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Name'),
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(labelText: 'Phone'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Address'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Relationship (e.g., spouse)"),
+                  ),
+                  TextFormField(
+                    decoration:
+                        InputDecoration(labelText: "Events (e.g., Birthday)"),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
